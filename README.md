d42c61053d3c26f6a3d969661059462ed048b774
spyoungtech <spenceryoung@ufl.edu>
2017-09-13 19:42:12 -0400

    bug fix. Doctests. readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # chunker
 tools for chunking iterables
+
+
+## chunker.split
+
+```py
+>>> a_list = ["foo", 'bar', 'SENTINEL', 'bacon', 'eggs']
+>>> split(a_list, 'SENTINEL')
+[['foo', 'bar'], ['bacon', 'eggs']]
+```
+
+## chunker.chunk
+
+```py
+>>> r = range(5)
+>>> for c in chunk(r, 2):
+...     print(a, b)
+(0, 1)
+(2, 3)
+(4,)
+```
--- chunker/chunker.py
+++ chunker/chunker.py
@@ -1,6 +1,31 @@
-def split(iterable, sentinel):
-    """Works like str.split, but with any arbitrary iterable"""
+def _iter(iterable, sentinel):
     x = iter(iterable)
+    for item in x:
+        yield item
+    yield sentinel
+
+
+def split(iterable, sentinel):
+    """
+    Split any arbitrary iterable into list items separated by a sentinel value.
+    :param iterable: any iterable
+    :param sentinel: value to split on
+    :returns: a list of chunks
+    >>> a_list = ["foo", 'bar', 'SENTINEL', 'bacon', 'eggs']
+    >>> split(a_list, 'SENTINEL')
+    [['foo', 'bar'], ['bacon', 'eggs']]
+    """
+    return list(iter_split(iterable, sentinel))
+
+def iter_split(iterable, sentinel):
+    """
+    Split any arbitrary iterable into list items separated by a sentinel value.
+
+    :param iterable: any iterable
+    :param sentinel: value to split on
+    :yields: chunks
+    """
+    x = _iter(iterable, sentinel)
     for item in x:
         chunk = []
         while item != sentinel:
@@ -9,8 +34,16 @@ def split(iterable, sentinel):
         yield chunk
 
 def chunk(iterable, chunk_size):
-    """Yield successive n-sized chunks from l."""
+    """
+    Yield successive n-sized chunks from an iterable.
+    If the iterable does not fit evenly into chunk_size, the last chunk will be smaller than chunk_size.
+    >>> r = range(5)
+    >>> for c in chunk(r, 2):
+    ...     print(a, b)
+    (0, 1)
+    (2, 3)
+    (4,)
+    """
     x = iter(iterable)
     for item in x:
         yield (item, *(next(x) for _ in range(chunk_size - 1)))
-
